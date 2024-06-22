32a3ed7b86b061fd198f02710665f7e53f0cded7
Yan Gonçalves <yanw100@gmail.com>
2024-06-22 20:49:02 -0300

    Carrega arquivo .env no início do script. - Melhora estrutura do código. - Adiciona mensagens de erro e de execução

--- main.go
+++ main.go
@@ -1,42 +1,98 @@
 package main
 
 import (
-    "encoding/csv"
-    "os"
+	"encoding/csv"
+	"fmt"
+	"github.com/gofor-little/env"
+	"log"
+	"os"
 )
 
 func main() {
-	file, err := os.Open("./files/ceps.csv")
-    if err != nil {
-        panic(err)
-    }
-	defer file.Close()
+	loadEnv()
+	fmt.Println("Iniciando script de busca de endereços")
+	records := getCEPs()
 
-	records, err := csv.NewReader(file).ReadAll()
-    if err != nil {
-        panic(err)
-    }
-
-	var cep []string
+	var ceps []string
 
 	for _, record := range records {
-		cep = append(cep, record[0])
+		ceps = append(ceps, record[0])
 	}
 
 	addressesFile, err := os.Create("./files/addresses.csv")
-    if err != nil {
-        panic(err)
-    }
-	defer addressesFile.Close()
+	if err != nil {
+		panic(err)
+	}
+	defer func(addressesFile *os.File) {
+		err := addressesFile.Close()
+		if err != nil {
+			log.Fatalf("Error closing file: %v", err)
+		}
+	}(addressesFile)
 
 	writer := csv.NewWriter(addressesFile)
 	defer writer.Flush()
 
-	ceptowrite := make([][]string, len(cep))
-	for i, c := range cep {
+	ceptowrite := make([][]string, len(ceps))
+	for i, c := range ceps {
+		// TODO: Implementar busca no google maps
 		ceptowrite[i] = []string{c}
 		if err := writer.Write(ceptowrite[i]); err != nil {
 			panic(err)
 		}
 	}
+	fmt.Println("Script finalizado")
 }
+
+func getCEPs() [][]string {
+	file, err := os.Open("./files/ceps.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("failed to close file: %s", err)
+		}
+	}(file)
+
+	records, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		panic(err)
+	}
+	return records
+}
+
+func loadEnv() {
+	err := env.Load(".env")
+	if err != nil {
+		log.Fatalf("Falha ao localizar arquivo .env encerrando script")
+	}
+}
+
+//func readJson() {
+//	// Your JSON string
+//	var jsonStr = []byte(`{
+//		"name": "John",
+//		"age": 30,
+//		"city": "New York",
+//		"social": {
+//			"twitter": "@john",
+//			"email": "john@example.com"
+//		}
+//	}`)
+//
+//	// Use a map of string keys to values of any type
+//	var result map[string]interface{}
+//
+//	// Unmarshal the JSON string into the map
+//	if err := json.Unmarshal(jsonStr, &result); err != nil {
+//		log.Fatal(err)
+//	}
+//	fmt.Println(result)
+//
+//	// Print the map
+//	for key, value := range result {
+//		fmt.Printf("%s: %v\n", key, value)
+//	}
+//}
