abb19002a81e411570627640926dcab26a5b95bc
Yan Gonçalves <yanw100@gmail.com>
2024-06-22 22:02:17 -0300

    Implementa busca no google e escrita no csv

--- main.go
+++ main.go
@@ -2,52 +2,68 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
-	"github.com/gofor-little/env"
+	"io"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/gofor-little/env"
+)
+
+const (
+	addressFilePath = "./files/addresses.csv"
+	cepFilePath     = "./files/ceps.csv"
 )
 
 func main() {
 	loadEnv()
-	fmt.Println("Iniciando script de busca de endereços")
+
+	log.Println("Starting address search script")
+
 	records := getCEPs()
-
 	var ceps []string
-
 	for _, record := range records {
 		ceps = append(ceps, record[0])
 	}
 
-	addressesFile, err := os.Create("./files/addresses.csv")
-	if err != nil {
-		panic(err)
-	}
+	writer, addressesFile := initAddressFile()
 	defer func(addressesFile *os.File) {
 		err := addressesFile.Close()
 		if err != nil {
 			log.Fatalf("Error closing file: %v", err)
 		}
 	}(addressesFile)
-
-	writer := csv.NewWriter(addressesFile)
 	defer writer.Flush()
 
-	ceptowrite := make([][]string, len(ceps))
-	for i, c := range ceps {
-		// TODO: Implementar busca no google maps
-		ceptowrite[i] = []string{c}
-		if err := writer.Write(ceptowrite[i]); err != nil {
-			panic(err)
+	log.Println("Starting search on google")
+
+	for _, cep := range ceps {
+		addressData := searchOnGoogle(cep)
+		if len(addressData.Results) == 0 {
+			log.Println("No results found for CEP", cep)
+			continue
 		}
+		address := extractAddress(cep, addressData)
+		writeToFile(writer, address)
 	}
-	fmt.Println("Script finalizado")
+
+	log.Println("Finished address search script")
+}
+
+func loadEnv() {
+	err := env.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 }
 
 func getCEPs() [][]string {
-	file, err := os.Open("./files/ceps.csv")
+	log.Println("Loading CEPs from file")
+	file, err := os.Open(cepFilePath)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error occurred while opening file: %v", err)
 	}
 	defer func(file *os.File) {
 		err := file.Close()
@@ -55,44 +71,70 @@ func getCEPs() [][]string {
 			log.Fatalf("failed to close file: %s", err)
 		}
 	}(file)
-
 	records, err := csv.NewReader(file).ReadAll()
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error occurred while reading file: %v", err)
 	}
 	return records
 }
 
-func loadEnv() {
-	err := env.Load(".env")
+func initAddressFile() (writer *csv.Writer, addressesFile *os.File) {
+	addressesFile, err := os.Create(addressFilePath)
 	if err != nil {
-		log.Fatalf("Falha ao localizar arquivo .env encerrando script")
+		log.Fatalf("Error occurred while creating file: %v", err)
+	}
+	writer = csv.NewWriter(addressesFile)
+	writer.Comma = ';'
+	return
+}
+
+func searchOnGoogle(c string) GoogleResponse {
+	url := env.Get("GOOGLE_MAPS_URL", "")
+	resp, err := http.Get(url + "?address=" + c + "&key=" + env.Get("GOOGLE_MAPS_API_KEY", ""))
+	if err != nil {
+		log.Fatalf("Error occurred while fetching data from Google: %v", err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatalf("Error closing body: %v", err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Request failed with status code: %d", resp.StatusCode)
+	}
+
+	var response GoogleResponse
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		log.Fatalf("Error occurred while decoding response: %v", err)
+	}
+
+	return response
+}
+
+func extractAddress(cep string, response GoogleResponse) *Address {
+	result := response.Results[0]
+	lat, lng := result.Geometry.Location.Lat, result.Geometry.Location.Lng
+
+	return &Address{
+		Cep:              cep,
+		Latitude:         lat,
+		Longitude:        lng,
+		FormattedAddress: result.FormattedAddress,
 	}
 }
 
-//func readJson() {
-//	// Your JSON string
-//	var jsonStr = []byte(`{
-//		"name": "John",
-//		"age": 30,
-//		"city": "New York",
-//		"social": {
-//			"twitter": "@john",
-//			"email": "john@example.com"
-//		}
-//	}`)
-//
-//	// Use a map of string keys to values of any type
-//	var result map[string]interface{}
-//
-//	// Unmarshal the JSON string into the map
-//	if err := json.Unmarshal(jsonStr, &result); err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Println(result)
-//
-//	// Print the map
-//	for key, value := range result {
-//		fmt.Printf("%s: %v\n", key, value)
-//	}
-//}
+func writeToFile(writer *csv.Writer, address *Address) {
+	lineToWrite := addressToStrings(address)
+
+	err := writer.Write(lineToWrite)
+	if err != nil {
+		log.Println("Error occurred while writing to file:", err)
+	}
+}
+
+func addressToStrings(address *Address) []string {
+	return []string{address.Cep, fmt.Sprintf("%f", address.Latitude), fmt.Sprintf("%f", address.Longitude), address.FormattedAddress}
+}
